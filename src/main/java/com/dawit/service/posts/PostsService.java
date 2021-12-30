34bd2236520a0ab8c5bbd0b9c31e80dd7982fcfd
dawit <dawit0905@gmail.com>
2021-12-31 02:02:07 +0900

    add Posts update logic and Test

--- src/main/java/com/dawit/service/posts/PostsService.java
+++ src/main/java/com/dawit/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.dawit.service.posts;
 
+import com.dawit.domain.posts.Posts;
 import com.dawit.domain.posts.PostsRepository;
+import com.dawit.web.dto.PostsResponseDto;
 import com.dawit.web.dto.PostsSaveRequestDto;
+import com.dawit.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
 
