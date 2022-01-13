2f9c8a82c19d46acadbfa867fe03c4c5a07e6170
dawit <dawit0905@gmail.com>
2022-01-14 00:40:55 +0900

    update SessionUser.java

--- src/main/java/com/dawit/config/auth/dto/SessionUser.java
+++ src/main/java/com/dawit/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.dawit.config.auth.dto;
 import com.dawit.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
